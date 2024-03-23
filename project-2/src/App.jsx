42f75dfa2fb3b79613ad2733fe4603a5b5456694
Mohammad Ahmad <m.ahmad8929@gmail.com>
2024-03-24 01:18:37 +0530

    Added project-2 components and form validation

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,14 +1,19 @@
 
 import Navbar from './components/Navbar/Navbar'
 import ContactHeader from './components/ContactHeader/ContactHeader'
+import ContactForm from "./components/ContactForm/ContactForm";
+
 import './App.css'
 
 const App = () => {
   return (
-    <>
+    <div>
       <Navbar />
-      <ContactHeader />
-    </>
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
   )
 }
 
